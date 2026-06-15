38c2a4bb8538e35a4d15bd5ecccc2724f5eea599
annasauz <anna_francesca_sauz@dlsu.edu.ph>
2026-06-15 12:24:03 +0800

    ITEM CLASS
     This class is responsible for representing an item that can be sold by the vending machine. This includes storing the item's name, price, and calorie information.

---
+++ Item.java
@@ -0,0 +1,58 @@
+
+ //Represents an item sold in the vending machine.
+ 
+public class Item {
+    private String name;
+    private double price;
+    private double calories; 
+
+    /**
+     * Creates an item.
+     *
+     * @param name Name of the item
+     * @param price Price of the item
+     * @param calories Calories of the item
+     */
+    public Item(String name, double price, double calories) {
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    // Setter
+    /**
+     * Updates the price of the item.
+     *
+     * @param price The new price
+     * @return true if the price was updated, false otherwise
+     */
+    public boolean setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+            return true;
+        }
+        return false;
+    }
+
+    
+    //Displays item information. ** can be improved later on
+     
+    public void displayItemInfo() {
+        System.out.println("Item: " + name);
+        System.out.println("Price: ₱" + price);
+        System.out.println("Calories: " + calories + " kcal");
+    }
+}
