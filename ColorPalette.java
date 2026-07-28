cf583ad1b1412cf04ef57ac68b0d352c46c8cb27
annasauz <anna_francesca_sauz@dlsu.edu.ph>
2026-07-28 10:04:42 +0800

    GUI color palette
    Added colors for the GUI theme.

---
+++ ColorPalette.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class ColorPalette {
+
+    public static final Color BACKGROUND = new Color(248, 244, 236);
+
+    public static final Color PANEL = new Color(255,255,255);
+
+    public static final Color BUTTON = new Color(141, 110, 99);
+
+    public static final Color BUTTON_TEXT = Color.WHITE;
+
+    public static final Color TITLE = new Color(62,39,35);
+
+}
