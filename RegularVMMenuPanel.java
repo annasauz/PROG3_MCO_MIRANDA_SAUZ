4c51c2d3d85711f267acef5bf0d0b5a05991a103
annasauz <anna_francesca_sauz@dlsu.edu.ph>
2026-08-02 19:24:19 +0800

    Improve class documentation
    - Add class-level JavaDoc comments describing inheritance and responsibilities

--- RegularVMMenuPanel.java
+++ RegularVMMenuPanel.java
@@ -8,6 +8,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * Represents the menu for accessing Regular Vending Machine features.
+ * Extends JPanel to provide purchase, maintenance, and navigation
+ * controls.
+ */
 public class RegularVMMenuPanel extends JPanel {
 
     /**
